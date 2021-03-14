9fe6fb2d119e4e5600602131be6c79e1976d948c
burachevsky <burachevsky666@gmail.com>
2021-03-14 16:41:10 +0300

    refactor binding expressions in list_item_news.xml

--- app/src/main/java/com/burachevsky/rssfeedreader/ui/adapters/NewsListBindingAdapters.kt
+++ app/src/main/java/com/burachevsky/rssfeedreader/ui/adapters/NewsListBindingAdapters.kt
@@ -1,13 +1,17 @@
 package com.burachevsky.rssfeedreader.ui.adapters
 
+import android.annotation.SuppressLint
+import android.text.format.DateFormat
 import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
-import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.burachevsky.rssfeedreader.R
 import com.burachevsky.rssfeedreader.data.domainobjects.NewsItem
+
 
 @BindingAdapter("setIconVisible")
 fun bindSetIconVisible(channelIcon: ImageView, item: NewsItem) {
@@ -17,4 +21,19 @@ fun bindSetIconVisible(channelIcon: ImageView, item: NewsItem) {
         .error(R.drawable.ic_feed)
         .diskCacheStrategy(DiskCacheStrategy.ALL)
         .into(channelIcon)
+}
+
+@BindingAdapter("formatDate")
+fun bindFormatDate(textView: TextView, item: NewsItem) {
+    textView.text = DateFormat.format("dd.MM hh:mm", item.pubDate)
+}
+
+@BindingAdapter("setTextColor")
+fun bindSetTextColor(textView: TextView, item: NewsItem) {
+    val ctx = textView.context
+    textView.setTextColor(
+        if (item.isRead)
+            ContextCompat.getColor(ctx, R.color.read_text)
+        else ContextCompat.getColor(ctx, R.color.unread_text)
+    )
 }
