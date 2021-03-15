b8bb98fdecb8721f9d2f2280fa0a8e2972be4788
burachevsky <burachevsky666@gmail.com>
2021-03-15 12:19:50 +0300

    remove list to string converters

--- app/src/main/java/com/burachevsky/rssfeedreader/utils/Converters.kt
+++ app/src/main/java/com/burachevsky/rssfeedreader/utils/Converters.kt
@@ -17,16 +17,4 @@ class Converters {
     fun dateToTimestamp(date: Date?): Long? {
         return date?.time
     }
-
-    @TypeConverter
-    fun stringToList(value: String?): List<String?> {
-        val listType: Type = object : TypeToken<ArrayList<String?>?>() {}.getType()
-        return Gson().fromJson(value, listType)
-    }
-
-    @TypeConverter
-    fun listToString(list: List<String?>?): String {
-        val gson = Gson()
-        return gson.toJson(list)
-    }
 }
